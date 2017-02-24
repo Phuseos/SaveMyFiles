c807c13d4c9325da6d8515d65c1a2c9b3d6f8264
Phuseos <Phuseos@gmail.com>
2017-02-24 16:39:55 +0100

    Initial commit

--- SaveMyFiles/Form1.Designer.cs
+++ SaveMyFiles/Form1.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtIntro_NonEnable = new System.Windows.Forms.TextBox();
+            this.btnFileBackup = new System.Windows.Forms.Button();
+            this.txtSelectedFileLocation = new System.Windows.Forms.TextBox();
+            this.lblNo_Interact_FileLocation = new System.Windows.Forms.Label();
+            this.btnBackupLocation = new System.Windows.Forms.Button();
+            this.lblNo_Interact_BackupLocation = new System.Windows.Forms.Label();
+            this.txtSelectedBackupLocation = new System.Windows.Forms.TextBox();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtIntro_NonEnable
+            // 
+            this.txtIntro_NonEnable.BackColor = System.Drawing.Color.White;
+            this.txtIntro_NonEnable.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtIntro_NonEnable.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtIntro_NonEnable.Enabled = false;
+            this.txtIntro_NonEnable.Font = new System.Drawing.Font("Calibri", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIntro_NonEnable.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtIntro_NonEnable.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtIntro_NonEnable.Location = new System.Drawing.Point(13, 13);
+            this.txtIntro_NonEnable.Name = "txtIntro_NonEnable";
+            this.txtIntro_NonEnable.Size = new System.Drawing.Size(958, 41);
+            this.txtIntro_NonEnable.TabIndex = 0;
+            this.txtIntro_NonEnable.Text = "Welcome to Save My Files! Press the button below to begin.";
+            this.txtIntro_NonEnable.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnFileBackup
+            // 
+            this.btnFileBackup.Location = new System.Drawing.Point(99, 103);
+            this.btnFileBackup.Name = "btnFileBackup";
+            this.btnFileBackup.Size = new System.Drawing.Size(147, 43);
+            this.btnFileBackup.TabIndex = 1;
+            this.btnFileBackup.Text = "Select file to backup ";
+            this.btnFileBackup.UseVisualStyleBackColor = true;
+            this.btnFileBackup.Click += new System.EventHandler(this.btnFileBackup_Click);
+            // 
+            // txtSelectedFileLocation
+            // 
+            this.txtSelectedFileLocation.Location = new System.Drawing.Point(329, 121);
+            this.txtSelectedFileLocation.Name = "txtSelectedFileLocation";
+            this.txtSelectedFileLocation.Size = new System.Drawing.Size(520, 20);
+            this.txtSelectedFileLocation.TabIndex = 2;
+            // 
+            // lblNo_Interact_FileLocation
+            // 
+            this.lblNo_Interact_FileLocation.AutoSize = true;
+            this.lblNo_Interact_FileLocation.Location = new System.Drawing.Point(329, 103);
+            this.lblNo_Interact_FileLocation.Name = "lblNo_Interact_FileLocation";
+            this.lblNo_Interact_FileLocation.Size = new System.Drawing.Size(65, 13);
+            this.lblNo_Interact_FileLocation.TabIndex = 3;
+            this.lblNo_Interact_FileLocation.Text = "Selected file";
+            // 
+            // btnBackupLocation
+            // 
+            this.btnBackupLocation.Location = new System.Drawing.Point(99, 175);
+            this.btnBackupLocation.Name = "btnBackupLocation";
+            this.btnBackupLocation.Size = new System.Drawing.Size(147, 43);
+            this.btnBackupLocation.TabIndex = 4;
+            this.btnBackupLocation.Text = "Select backup location";
+            this.btnBackupLocation.UseVisualStyleBackColor = true;
+            this.btnBackupLocation.Click += new System.EventHandler(this.btnBackupLocation_Click);
+            // 
+            // lblNo_Interact_BackupLocation
+            // 
+            this.lblNo_Interact_BackupLocation.AutoSize = true;
+            this.lblNo_Interact_BackupLocation.Location = new System.Drawing.Point(329, 175);
+            this.lblNo_Interact_BackupLocation.Name = "lblNo_Interact_BackupLocation";
+            this.lblNo_Interact_BackupLocation.Size = new System.Drawing.Size(84, 13);
+            this.lblNo_Interact_BackupLocation.TabIndex = 6;
+            this.lblNo_Interact_BackupLocation.Text = "Backup location";
+            // 
+            // txtSelectedBackupLocation
+            // 
+            this.txtSelectedBackupLocation.Location = new System.Drawing.Point(329, 193);
+            this.txtSelectedBackupLocation.Name = "txtSelectedBackupLocation";
+            this.txtSelectedBackupLocation.Size = new System.Drawing.Size(520, 20);
+            this.txtSelectedBackupLocation.TabIndex = 5;
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackup.Location = new System.Drawing.Point(329, 272);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(279, 83);
+            this.btnBackup.TabIndex = 7;
+            this.btnBackup.Text = "Save My Files!";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(329, 148);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(100, 20);
+            this.txtFileName.TabIndex = 8;
+            this.txtFileName.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(983, 448);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.lblNo_Interact_BackupLocation);
+            this.Controls.Add(this.txtSelectedBackupLocation);
+            this.Controls.Add(this.btnBackupLocation);
+            this.Controls.Add(this.lblNo_Interact_FileLocation);
+            this.Controls.Add(this.txtSelectedFileLocation);
+            this.Controls.Add(this.btnFileBackup);
+            this.Controls.Add(this.txtIntro_NonEnable);
+            this.Name = "Form1";
+            this.Text = "Save My Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtIntro_NonEnable;
+        private System.Windows.Forms.Button btnFileBackup;
+        private System.Windows.Forms.TextBox txtSelectedFileLocation;
+        private System.Windows.Forms.Label lblNo_Interact_FileLocation;
+        private System.Windows.Forms.Button btnBackupLocation;
+        private System.Windows.Forms.Label lblNo_Interact_BackupLocation;
+        private System.Windows.Forms.TextBox txtSelectedBackupLocation;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.TextBox txtFileName;
     }
 }
 
--- SaveMyFiles/Form1.cs
+++ SaveMyFiles/Form1.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+    Creates full backups of chosen files / folders to a selected location
+    Later: Remember backups made and restore when asked
+           Repeated backup creation based on schedule
+*/
+
 namespace SaveMyFiles
 {
     public partial class Form1 : Form
@@ -15,6 +21,72 @@ namespace SaveMyFiles
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnFileBackup_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofDiag = new OpenFileDialog();
+            
+            //Show the dialog 
+            DialogResult dRes = ofDiag.ShowDialog();
+
+            string fPath = null;
+            string FileName = null;
+
+            if (dRes == DialogResult.OK)
+            {
+                fPath = ofDiag.FileName;
+                FileName = System.IO.Path.GetFileName(ofDiag.FileName);
+            }
+
+            //Now that we have the filename and path, load it into the textbox
+            txtSelectedFileLocation.Text = fPath;
+            txtFileName.Text = FileName;
+        }
+
+        private void btnBackupLocation_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofDiag = new OpenFileDialog();
+
+            string fPath = null;
+
+            //Show the dialog
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrEmpty(fbd.SelectedPath))
+                {
+                    fPath = fbd.SelectedPath;
+                }
+                else
+                {
+                    MessageBox.Show("You have not selected a folder to write the document to.");
+                    return;
+                }
+            }
+
+            //Now that we have the filename and path, load it into the textbox
+            txtSelectedBackupLocation.Text = fPath;
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSelectedFileLocation.Text))
+            {
+                MessageBox.Show("There's no file to backup selected, do this first.");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtSelectedBackupLocation.Text))
+            {
+                MessageBox.Show("There's no backup location selected, do this first.");
+                return;
+            }
+
+            //Now backup the file
+            System.IO.File.Copy(txtSelectedFileLocation.Text, txtSelectedBackupLocation.Text + "/" + txtFileName.Text + ".BACKUP");
+
+            MessageBox.Show("File succesfully backed as " + txtSelectedBackupLocation.Text + "/" + txtFileName.Text + ".BACKUP");
         }
     }
 }
